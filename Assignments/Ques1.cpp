77449da2e45951f385116a3df73ce045366e9aa8
Yug Malhotra <yug25csu348@ncuindia.edu>
2026-05-08 21:53:08 +0530

    Create Ques1.cpp

---
+++ Assignments/Ques1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x,y,z;
+    cin >> x >> y >> z;
+    float avg = (x + y + z) / 3.0;
+    cout << "Average: " << avg << endl;
+    return 0;
+}
