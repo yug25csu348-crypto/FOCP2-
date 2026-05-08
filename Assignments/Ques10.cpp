a5262568fd9a2bfb46efeeeb7255c86dfa986f01
Yug Malhotra <yug25csu348@ncuindia.edu>
2026-05-08 22:01:54 +0530

    Create Ques10.cpp

---
+++ Assignments/Ques10.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+    int year;
+    cout << "Enter a year: ";
+    cin >> year;
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+        cout << year << " it is a leap year." << endl;
+    } else {
+        cout << year << " is not a leap year." << endl;
+    }
+  
+    return 0;
+}
