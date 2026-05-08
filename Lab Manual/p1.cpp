ed47eceb6dc5c7014d5635a17959c1bbde580d87
Yug Malhotra <yug25csu348@ncuindia.edu>
2026-05-08 22:37:23 +0530

    Create p1.cpp

---
+++ Lab Manual/p1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+#include<string>
+int main() {
+	int num;
+	cin >> num;    //Reading input from STDIN
+    string str;
+    cin >> str;
+	cout << num*2 << endl;	// Writing output to STDOUT
+    cout << str << endl;
+    return 0;
+}
