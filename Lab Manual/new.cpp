56631925b1dc7a0c78b5ca8ddc7c0d489e947ade
Yug Malhotra <yug25csu348@ncuindia.edu>
2026-05-09 13:10:01 +0530

    Create new.cpp

---
+++ Lab Manual/new.cpp
@@ -0,0 +1,13 @@
+#include "iostream"
+using namespace std;
+
+int main(){
+    int a,b;
+    cout << "enter two numbers";
+    cin >> a >> b;
+
+    cout << "Result = " << a / b << endl;
+    cout << "End of program" << endl;
+    
+    return 0;   
+}
